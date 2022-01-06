e02662b4ee432b484e8cf587d5743bdbd5819a0d
YuanL12 <yuanluo@uchicago.edu>
2022-01-06 05:41:27 +0000

    add sparse rips

--- torch_tda/nn/functional/rips.py
+++ torch_tda/nn/functional/rips.py
@@ -3,6 +3,10 @@ import torch
 from torch.autograd import Function
 import scipy.spatial.distance as distance
 from .util import dgms_tensor_list
+import numpy as np
+from sklearn.metrics.pairwise import pairwise_distances
+import time
+from scipy import sparse
 
 def compute_y_gradient(X, F, R, imap, grad_dgms):
     '''
@@ -64,6 +68,129 @@ def compute_y_gradient(X, F, R, imap, grad_dgms):
 
     return y_grad
 
+def getGreedyPerm(D):
+    """
+    A Naive O(N^2) algorithm to do furthest points sampling
+
+    Parameters
+    ----------
+    D : ndarray (N, N)
+        An NxN distance matrix for points
+
+    Return
+    ------
+    lamdas: list
+        Insertion radii of all points
+    """
+
+    N = D.shape[0]
+    #By default, takes the first point in the permutation to be the
+    #first point in the point cloud, but could be random
+    perm = np.zeros(N, dtype=np.int64)
+    lambdas = np.zeros(N)
+    ds = D[0, :]
+    for i in range(1, N):
+        idx = np.argmax(ds)
+        perm[i] = idx
+        lambdas[i] = ds[idx]
+        ds = np.minimum(ds, D[idx, :])
+    return lambdas[perm]
+
+def getApproxSparseDM(lambdas, eps, D):
+    """
+    Purpose: To return the sparse edge list with the warped distances, sorted by weight
+
+    Parameters
+    ----------
+    lambdas: list
+        insertion radii for points
+    eps: float
+        epsilon approximation constant
+    D: ndarray
+        NxN distance matrix, okay to modify because last time it's used
+
+    Return
+    ------
+    DSparse: scipy.sparse
+        A sparse NxN matrix with the reweighted edges
+    """
+    N = D.shape[0]
+    E0 = (1+eps)/eps
+    E1 = (1+eps)**2/eps
+
+    # Create initial sparse list candidates (Lemma 6)
+    # Search neighborhoods
+    nBounds = ((eps**2+3*eps+2)/eps)*lambdas
+
+    # Set all distances outside of search neighborhood to infinity
+    D[D > nBounds[:, None]] = np.inf
+    [I, J] = np.meshgrid(np.arange(N), np.arange(N))
+    idx = I < J
+    I = I[(D < np.inf)*(idx == 1)]
+    J = J[(D < np.inf)*(idx == 1)]
+    D = D[(D < np.inf)*(idx == 1)]
+
+    #Prune sparse list and update warped edge lengths (Algorithm 3 pg. 14)
+    minlam = np.minimum(lambdas[I], lambdas[J])
+    maxlam = np.maximum(lambdas[I], lambdas[J])
+
+    # Rule out edges between vertices whose balls stop growing before they touch
+    # or where one of them would have been deleted.  M stores which of these
+    # happens first
+    M = np.minimum((E0 + E1)*minlam, E0*(minlam + maxlam))
+
+    t = np.arange(len(I))
+    t = t[D <= M]
+    (I, J, D) = (I[t], J[t], D[t])
+    minlam = minlam[t]
+    maxlam = maxlam[t]
+
+    # Now figure out the metric of the edges that are actually added
+    t = np.ones(len(I))
+
+    # If cones haven't turned into cylinders, metric is unchanged
+    t[D <= 2*minlam*E0] = 0
+
+    # Otherwise, if they meet before the M condition above, the metric is warped
+    D[t == 1] = 2.0*(D[t == 1] - minlam[t == 1]*E0) # Multiply by 2 convention
+    return sparse.coo_matrix((D, (I, J)), shape=(N, N)).tocsr()
+
+def sparse_pairwise_dist(D, eps = 0.15, dense_output=False):
+    '''
+    Generate Sparse pairwise distance matrix. In principal, the idea is to
+    1) rule out of egdes that will never be added in Sparse Rips
+    2) modify edges' length that will become 'cylinder' as described in paper
+    
+    Note:
+    As eps increase, the compression rate will increase.
+    
+    Input
+    ------
+    D: N * N numpy arrary 
+        pairwise distance matrix
+    
+    eps: float
+        perisitent diagrams approximation bound
+    '''
+    
+    t0 = time.monotonic()
+    # First do furthest point sampling
+    lambdas = getGreedyPerm(D)
+    # Now compute the sparse distance matrix
+    DSparse = getApproxSparseDM(lambdas, eps, D)
+    t1 = time.monotonic()
+    if dense_output:
+        print("sparse Rips construction takes {} sec.".format(t1 - t0))
+        print("{} left with edges".format(DSparse.nnz))
+        print("{:.2f}% edges compression".format((1 - DSparse.nnz / np.prod(D.shape))*100))
+    
+    # return dense distance matrix
+    D_dense = DSparse.todense()
+    # since BATs will not consider diagonal elements when construct filtration
+    # we will simply just the diagonal line to 0
+    D_dense[D_dense == 0] = np.inf 
+    rad = DSparse.max() + eps # radius
+    return D_dense, rad
 
 class RipsDiagram(Function):
     """
@@ -71,15 +198,22 @@ class RipsDiagram(Function):
     forward inputs:
         y - N x D torch.float tensor of coordinates (original data points)
         maxdim - maximum homology dimension
+        sparse - True if you want to use sparse Rips
+        eps - approximation error bound of bottleneck distance for sparse Rips
         reduction_flags - optional reduction flags for BATS.
     """
     @staticmethod
-    def forward(ctx, y, maxdim, *reduction_flags):
+    def forward(ctx, y, maxdim, sparse=False, eps=0.5, *reduction_flags):
         # number of arguments should match the return of backward function
 
         ynp = y.detach().numpy()
-        DX = distance.squareform(distance.pdist(ynp))
-        rX = bats.enclosing_radius(bats.Matrix(DX))
+        if sparse:
+            D = pairwise_distances(ynp, metric='euclidean')
+            DX, rX = sparse_pairwise_dist(D, eps, dense_output = False)
+        else:
+            DX = distance.squareform(distance.pdist(ynp))
+            rX = bats.enclosing_radius(bats.Matrix(DX))
+        
         # maixmum complex dimension = maximum homology dimension + 1
         F, imap = bats.LightRipsFiltration_extension(bats.Matrix(DX), rX , maxdim+1)
         R = bats.reduce(F, bats.F2(), *reduction_flags)
@@ -124,7 +258,7 @@ class RipsDiagram(Function):
         grad_y = compute_y_gradient(ycpu, F, R, imap, grad_dgms)
 
         # backward only to the first argument in inputs: y 
-        ret = [grad_y.to(device), None] 
+        ret = [grad_y.to(device), None, None, None] 
         # 'None' here means that we need to match the forward function arguments
         # and `maxdim` and `*reduction_flags` cannot do gradient-descent
         ret.extend([None for f in ctx.reduction_flags])
--- torch_tda/nn/rips.py
+++ torch_tda/nn/rips.py
@@ -11,15 +11,17 @@ class RipsLayer(nn.Module):
             see details in:
             https://bats-tda.readthedocs.io/en/latest/tutorials/Rips.html#Algorithm-optimization
     """
-    def __init__(self, maxdim = 0, reduction_flags=()):
+    def __init__(self, maxdim = 0, sparse = False, eps=0.5,  reduction_flags=()):
         super(RipsLayer, self).__init__()
         self.maxdim = maxdim
+        self.sparse = sparse
+        self.eps = eps
         self.PD = RipsDiagram()
         self.reduction_flags = reduction_flags
 
     def forward(self, x):
         xnp = x.cpu().detach().numpy() # convert to numpy array
-        dgms = self.PD.apply(x, self.maxdim, *self.reduction_flags)
+        dgms = self.PD.apply(x, self.maxdim, self.sparse, self.eps, *self.reduction_flags)
         return dgms
 
 
